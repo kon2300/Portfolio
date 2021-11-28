be881667205b88e423843b6d37cbd13fd0e8e76f
kon2300 <yyaamm.2300@gmail.com>
2021-11-28 14:10:57 +0900

    :sparkles: Creating a CircleCI environment

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ set -ex
 SECURITY_GROUP_NAME="ec2-circleci"
 IP=`curl -f -s ifconfig.me`
 
-trap "aws ec2 revoke-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}/32" 0 1 2 3 15
-aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}/32
+aws ec2 authorize-security-group-ingress --group-id ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}/32
 ssh "cd Portfolio && git pull origin main"
+aws ec2 revoke-security-group-ingress --group-id ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}/32
